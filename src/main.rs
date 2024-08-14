7f666b74914c2bf1728250d7c66e44657ca14688
crackhex <troike@arizona.edu>
2024-08-14 03:39:54 -0700

    change necessary byte arrays to be Strings

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod m64_handling;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
-use crate::m64_handling::{BytesToAscii, M64Header, M64File};
+use crate::m64_handling::{M64File, bytes_to_ascii};
 
 fn open_file(path: &str) -> io::Result<File> {
     let f = File::open(path)?;
@@ -10,12 +10,12 @@ fn open_file(path: &str) -> io::Result<File> {
 }
 
 pub fn main() -> io::Result<()>{
-    let m64_path = "PATH\\TO\\FILE";
+    let m64_path = "C:\\Users\\austi\\Desktop\\ok\\TWDE";
     let m64_name = "gogg";
     let m64_file_path = format!("{}\\{}.m64", m64_path, m64_name);
     let mut file = open_file(&m64_file_path)?;
     let m64 = M64File::build_m64(&mut file).unwrap();
-    let internal_name = M64Header::bytes_to_ascii(&m64.header.internal_name);
+    let internal_name = &m64.header.internal_name;
     println!("{}", internal_name);
     Ok(())
 }
