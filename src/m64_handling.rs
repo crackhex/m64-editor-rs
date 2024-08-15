c8ee0a1c3339bbc61dd3127339258fa414e1005b
crackhex <troike@arizona.edu>
2024-08-15 13:42:14 -0700

    change inputs from Vec<Input> to [Vec<Input; 4] to capture controllers

--- src/m64_handling.rs
+++ src/m64_handling.rs
@@ -3,6 +3,7 @@ use std::ascii::Char as AsciiChar;
 use std::fs::File;
 use std::io::Read;
 use std::ops::{Shr};
+use bitvec::prelude::*;
 
 pub(crate) struct M64File {
     pub signature: [u8; 4],                 //0x00 4 bytes
@@ -24,10 +25,10 @@ pub(crate) struct M64File {
     pub rsp_plugin: [AsciiChar; 64],        //0x1E2 64 bytes
     pub author: [AsciiChar; 222],           //0x222 220 bytes
     pub movie_desc: [AsciiChar; 256],       //0x300 256 bytes
-    pub inputs: Vec<InputLayout>,                   //0x400
+    pub inputs: [Vec<Input>; 4],            //0x400
 }
 
-pub(crate) struct InputLayout {
+pub(crate) struct Input {
     pub r_dpad: bool,
     pub l_dpad: bool,
     pub d_dpad: bool,
@@ -46,12 +47,20 @@ pub(crate) struct InputLayout {
     pub y: i8,
 }
 
-impl InputLayout {
-    fn parse_inputs(inputs: &Vec<u8>) -> Vec<InputLayout> {
-        let mut input_layouts: Vec<InputLayout> = vec![];
-        for i in (0..inputs.len()-4).step_by(4) {
-            let input = u32::from_le_bytes(inputs[i..i+4].try_into().unwrap());
-            input_layouts.push(InputLayout {
+impl Input {
+    fn parse_inputs(input_bytes: &Vec<u8>, controller_flags: u8) -> [Vec<Input>; 4] {
+        let mut inputs: [Vec<Input>; 4] = [const { Vec::new() }; 4];
+        let mut active_controllers = vec![];
+        let controllers: BitArray<u8>= controller_flags.into_bitarray();
+        for i in 0..4 {
+            if controllers[i] {
+                active_controllers.push(i);
+            }
+        };
+        for i in (0..input_bytes.len()-4).step_by(4) {
+            let input = u32::from_le_bytes(input_bytes[i..i+4].try_into().unwrap());
+            let current_controller = active_controllers[i % active_controllers.len()];
+            inputs[current_controller].push(Input {
                 r_dpad: (input & 0x01) != 0,
                 l_dpad: (input & 0x02) != 0,
                 d_dpad: (input & 0x04) != 0,
@@ -70,7 +79,7 @@ impl InputLayout {
                 y: input.shr(24) as i8,
             });
         }
-        input_layouts
+        inputs
     }
 }
 
@@ -96,7 +105,7 @@ impl M64File {
             rsp_plugin: [0_u8.as_ascii().unwrap(); 64],
             author: [0_u8.as_ascii().unwrap(); 222],
             movie_desc: [0_u8.as_ascii().unwrap(); 256],
-            inputs: vec![],
+            inputs: [const { Vec::new() }; 4],
         }
     }
     pub fn build_m64(f: &mut File) -> Result<M64File, TryFromSliceError> {
@@ -107,6 +116,7 @@ impl M64File {
         let mut buffer: Vec<u8> = vec![0; m64_len];
 
         f.read(&mut buffer[..]).expect("TODO: panic message");
+
         let mut m64 = M64File {
             signature: buffer[0x0..0x4].try_into()?,
             version: u32::from_le_bytes(buffer[0x4..0x8].try_into()?),
@@ -127,7 +137,7 @@ impl M64File {
             rsp_plugin: *<&[u8] as TryInto<[u8; 64]>>::try_into(&buffer[0x1E2..0x222])?.as_ascii().unwrap(),
             author: *<&[u8] as TryInto<[u8; 222]>>::try_into(&buffer[0x222..0x300])?.as_ascii().unwrap(),
             movie_desc: *<&[u8] as TryInto<[u8; 256]>>::try_into(&buffer[0x300..0x400])?.as_ascii().unwrap(),
-            inputs: InputLayout::parse_inputs(&buffer[0x400..].to_vec()),
+            inputs: Input::parse_inputs(&buffer[0x400..].to_vec(), buffer[0x20]),
         };
 
         Ok(m64)
